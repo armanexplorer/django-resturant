00fd36f7553b9ea6120f8d11f0665ac74c7ea6a2
Arman <armanexplorer@gmail.com>
2024-11-04 08:46:24 +0330

    refactor: remove extra comments and fix tests structure

--- project/order/admin.py
+++ project/order/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from order.models import Item, Order, OrderItem
 
 admin.site.register([Order, OrderItem, Item])
+
+# TODO: we can define customized ModelAdmin for each of these model for better
+#  monitoring on them
--- project/order/signals.py
+++ project/order/signals.py
@@ -4,17 +4,8 @@ from django.dispatch import receiver
 
 from .models import Order
 
-# from .utils import update_inventory
-
 
 @receiver(post_save, sender=Order)
 def notify_order_total_price(sender, instance, created, **kwargs):
     if instance.total_price and instance.total_price > 50:
         print(f"SMS sent to {instance.phone}")
-
-
-# @receiver(post_save, sender=Order)
-# def update_inventory_after_order_delivery(sender, instance, created, **kwargs):
-#     if instance.status == 'delivered':
-#         for order_item in instance.order_items.all():
-#             update_inventory(order_item.name, order_item.count)
